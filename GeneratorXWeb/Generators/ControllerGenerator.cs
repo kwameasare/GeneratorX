e1b27b78befc79b331c1f5abf489ce2b75f93519
Praise <ipraiseasare@gmail.com>
2023-06-24 23:47:24 +0000

    update

--- GeneratorXWeb/Generators/ControllerGenerator.cs
+++ GeneratorXWeb/Generators/ControllerGenerator.cs
@@ -1,6 +1,64 @@
-﻿namespace GeneratorXWeb.Generators
+﻿public class ControllerGenerator
 {
-    public class ControllerGenerator
+    private readonly ClassBuilder _classBuilder;
+    private readonly string _projectName;
+    private readonly string _featureName;
+
+    public ControllerGenerator(string projectName, string featureName)
     {
+        _classBuilder = new ClassBuilder();
+        _projectName = projectName;
+        _featureName = featureName;
+    }
+
+    public string Generate()
+    {
+        _classBuilder
+            .AddUsingDirective("System")
+            .AddUsingDirective("System.Collections.Generic")
+            .AddUsingDirective("System.Linq")
+            .AddUsingDirective("System.Threading.Tasks")
+            .AddUsingDirective($"{_projectName}.Extensions")
+            .AddUsingDirective($"{_projectName}.Models.ModuleClaims")
+            .AddUsingDirective($"{_projectName}.Models.ViewModels")
+            .AddUsingDirective($"{_projectName}.Services.IServices")
+            .AddUsingDirective("Microsoft.AspNetCore.Authorization")
+            .AddUsingDirective("Microsoft.AspNetCore.Http")
+            .AddUsingDirective("Microsoft.AspNetCore.Mvc")
+            .StartNamespace($"{_projectName}.Areas.Masters.Controllers")
+            .StartClassDeclaration($"{_featureName}Controller", "Controller")
+            .AddPrivateReadonlyField($"I{_featureName}Service", $"_{_featureName.ToLower()}Service")
+            .StartConstructor($"{_featureName}Controller", $"I{_featureName}Service {_featureName.ToLower()}Service")
+            .AssignFieldFromParameter($"{_featureName.ToLower()}Service")
+            .EndAction()
+            .AddAttribute("HttpGet")
+            .AddMethod("IActionResult", "Index", $"ViewData[\"{_featureName.ToLower()}\"] = \"active\";\n\nreturn View();")
+            .AddAttribute("HttpGet")
+            .AddMethod("IActionResult", "GetGridList", $"var viewModel = _{_featureName.ToLower()}Service.Get{_featureName}List();\n\nreturn PartialView(viewModel);")
+            .AddAttribute("HttpGet")
+            .AddAttribute("Authorize", $"(Permissions.{_featureName}.Save)")
+            .AddMethod("IActionResult", "Add", $"ViewBag.detailTitle = \"Add {_featureName}\";\n\nreturn PartialView(new {_featureName}AddEditViewModel {{ Guid = UniqueKeys.GUIDEncryptionMask()}});")
+            .AddAttribute("HttpPost")
+            .AddAttribute("Authorize", $"(Permissions.{_featureName}.Save)")
+            .AddMethod("IActionResult", "Add", $"{_featureName}AddEditViewModel viewModel", $"if(ModelState.IsValid)\n{{\n    viewModel.AppUserId = HttpContext.Session.Get<long>(SessionValueKeys.appUserId);\n    _{_featureName.ToLower()}Service.Add{_featureName}(viewModel);\n\n    return Content(\"Successfully added {_featureName.ToLower()}\");\n}}\n\nthrow new BaseException(GlobalFunctions.GetModelStateErrors(ModelState));")
+            .AddAttribute("HttpGet")
+            .AddAttribute("Authorize", $"(Permissions.{_featureName}.Edit)")
+            .AddMethod("IActionResult", "Update", "int sectorId", $"ViewBag.detailTitle = \"Edit {_featureName}\";\n\nvar viewModel = _{_featureName.ToLower()}Service.GetUpdateViewModel(sectorId);\n\nreturn PartialView(viewModel);")
+            .AddAttribute("HttpPost")
+            .AddAttribute("Authorize", $"(Permissions.{_featureName}.Edit)")
+            .AddMethod("IActionResult", "Update", $"{_featureName}AddEditViewModel viewModel", $"if (ModelState.IsValid)\n{{\n    viewModel.AppUserId = HttpContext.Session.Get<long>(SessionValueKeys.appUserId);\n    _{_featureName.ToLower()}Service.Update{_featureName}(viewModel);\n\n    return Content(\"Successfully edited {_featureName.ToLower()}\");\n}}\n\nthrow new BaseException(GlobalFunctions.GetModelStateErrors(ModelState));")
+            .AddAttribute("HttpDelete")
+            .AddAttribute("Authorize", $"(Permissions.{_featureName}.DeleteAction)")
+            .AddMethod("IActionResult", "Delete", "int sectorId", $"{_featureName}DeleteViewModel viewModel = new {_featureName}DeleteViewModel\n{{\n    SectorId = sectorId,\n    AppUserId = HttpContext.Session.Get<long>(SessionValueKeys.appUserId)\n}};\n\n_{_featureName.ToLower()}Service.Delete{_featureName}(viewModel);\n\nreturn Content(\"Successfully deleted {_featureName.ToLower()}\");")
+            .AddAttribute("HttpGet")
+            .AddAttribute("Authorize", $"(Permissions.{_featureName}.Authorize)")
+            .AddMethod("IActionResult", "ApproveRecord", "int recordId", "ApprovalViewModel viewModel = new ApprovalViewModel\n{\n    RecordId = recordId,\n};\nViewBag.detailTitle = \"Authorize Record\";\n\nreturn PartialView(viewModel);")
+            .AddAttribute("HttpPost")
+            .AddAttribute("Authorize", $"(Permissions.{_featureName}.Authorize)")
+            .AddMethod("IActionResult", "ApproveRecord", "ApprovalViewModel viewModel", "if (ModelState.IsValid)\n{\n    viewModel.AppUserId = HttpContext.Session.Get<long>(SessionValueKeys.appUserId);\n    _{_featureName.ToLower()}Service.Approve{_featureName}(viewModel);\n\n    return Content($\"Successfully {(viewModel.IsApproved ? \"approved\" : \"disapproved\")} record\");\n}\n\nthrow new BaseException(GlobalFunctions.GetModelStateErrors(ModelState));")
+            .EndAction()
+            .EndAction();
+
+        return _classBuilder.ToString();
     }
 }
--- GeneratorXWeb/Helpers/ClassBuilder.cs
+++ GeneratorXWeb/Helpers/ClassBuilder.cs
@@ -1,45 +1,101 @@
 ﻿using System.Text;
 
-namespace GeneratorXWeb.Helpers
+public class ClassBuilder
 {
-    internal class ClassBuilder
+    private StringBuilder _builder;
+    private int _indentLevel;
+
+    public ClassBuilder()
     {
-        private readonly StringBuilder _result = new StringBuilder();
-        private int _indent = 0;
+        _builder = new StringBuilder();
+        _indentLevel = 0;
+    }
 
-        internal void AddLine() => AddLine("");
+    private void AppendIndentedLine(string line)
+    {
+        _builder.AppendLine(new string('\t', _indentLevel) + line);
+    }
 
-        internal void AddLine(string line)
-        {
-            for (int i = 0; i < _indent; i++)
-            {
-                _result.Append("    ");
-            }
+    public ClassBuilder AddUsingDirective(string namespaceName)
+    {
+        _builder.AppendLine($"using {namespaceName};");
+        return this;
+    }
 
-            _result.Append(line);
-            _result.Append(Environment.NewLine);
-        }
+    public ClassBuilder StartNamespace(string namespaceName)
+    {
+        AppendIndentedLine($"namespace {namespaceName}");
+        AppendIndentedLine("{");
+        _indentLevel++;
+        return this;
+    }
 
-        internal void AddLines(IEnumerable<string> lines)
-        {
-            foreach (var line in lines)
-            {
-                AddLine(line);
-            }
-        }
+    public ClassBuilder EndAction()
+    {
+        _indentLevel--;
+        AppendIndentedLine("}");
+        return this;
+    }
 
-        internal void Indent() => _indent++;
-        internal void Dedent() => _indent--;
+    public ClassBuilder StartClassDeclaration(string className, string baseClassName = null, bool isPartial = false)
+    {
+        AppendIndentedLine($"public {(isPartial ? "partial " : "")}class {className} : {baseClassName ?? "object"}");
+        AppendIndentedLine("{");
+        _indentLevel++;
+        return this;
+    }
 
-        public static ClassBuilder operator +(ClassBuilder builder, string str)
-        {
-            builder.AddLine(str);
-            return builder;
-        }
 
-        public override string ToString()
-        {
-            return _result.ToString();
-        }
+
+    public ClassBuilder AddPrivateReadonlyField(string typeName, string fieldName)
+    {
+        AppendIndentedLine($"private readonly {typeName} {fieldName};");
+        return this;
+    }
+
+    public ClassBuilder AddProperty(string typeName, string propertyName)
+    {
+        AppendIndentedLine($"public {typeName} {propertyName} {{ get; set; }}");
+        return this;
+    }
+
+    public ClassBuilder StartConstructor(string constructorName, string parameters)
+    {
+        AppendIndentedLine($"public {constructorName}({parameters})");
+        AppendIndentedLine("{");
+        _indentLevel++;
+        return this;
+    }
+
+    public ClassBuilder AssignFieldFromParameter(string parameterName)
+    {
+        AppendIndentedLine($"this.{parameterName} = {parameterName};");
+        return this;
+    }
+
+
+
+    public ClassBuilder AddMethod(string returnType, string methodName, string body, string parameterList = "", bool isPrivate = false)
+    {
+        AppendIndentedLine($"{(isPrivate ? "private" : "public")} {returnType} {methodName}({parameterList})");
+        AppendIndentedLine("{");
+        _indentLevel++;
+        AppendIndentedLine(body);
+        _indentLevel--;
+        AppendIndentedLine("}");
+        return this;
+    }
+
+
+
+    public ClassBuilder AddAttribute(string attributeName, string attributeParameters = "")
+    {
+        AppendIndentedLine($"[{attributeName}({attributeParameters})]");
+        return this;
+    }
+
+    public override string ToString()
+    {
+        return _builder.ToString();
     }
 }
